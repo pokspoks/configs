fdcb562f038236e995b760eefd5eda2a4185d05b
Poks <tehepicgames@gmail.com>
2024-04-17 21:30:33 +0300

    Add lazy and plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -40,3 +40,42 @@ vim.api.nvim_create_autocmd({"BufEnter"}, {
         vim.fn.system({"wezterm", "cli", "set-tab-title", title})
     end,
 })
+
+
+-- lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{
+	    "kylechui/nvim-surround",
+	    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+	    event = "VeryLazy",
+	    config = function()
+		require("nvim-surround").setup({
+		    -- Configuration here, or leave empty to use defaults
+		})
+	    end
+	},
+	{
+		'nvim-telescope/telescope.nvim', branch = '0.1.x',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+})
+
+-- Plugin keymaps
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
